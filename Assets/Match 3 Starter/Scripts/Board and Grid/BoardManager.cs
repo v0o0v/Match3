4ffbcd1487722a555b53194f04189645e4ac5fcb
v0o0v <v0o0v@daum.net>
2020-03-13 18:43:51 +0900

    게임 보드 초기화

--- Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -44,13 +44,28 @@ public class BoardManager : MonoBehaviour {
 	private void CreateBoard (float xOffset, float yOffset) {
 		tiles = new GameObject[xSize, ySize];
 
-        float startX = transform.position.x;
+		float startX = transform.position.x;
 		float startY = transform.position.y;
-
+		
+		Sprite[] previousLeft = new Sprite[ySize];
+		Sprite previousBelow = null;
+		
 		for (int x = 0; x < xSize; x++) {
 			for (int y = 0; y < ySize; y++) {
 				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
 				tiles[x, y] = newTile;
+				newTile.transform.parent = transform;
+				
+				List<Sprite> possibleCharacters = new List<Sprite>(); // 1
+				possibleCharacters.AddRange(characters); // 2
+				possibleCharacters.Remove(previousLeft[y]); // 3
+				possibleCharacters.Remove(previousBelow);
+				
+				Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+				newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+				
+				previousLeft[y] = newSprite;
+				previousBelow = newSprite;
 			}
         }
     }
